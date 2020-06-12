3ce26cbaf0aa927f3fb8342495cfa8c46eea0764
Oscar Martinez Sanchez <omsz90@live.com.mx>
2020-06-11 19:21:01 -0500

    :white_check_mark: adding some test
    add test

---
+++ index.test.js
@@ -0,0 +1,11 @@
+var isOms = require('./index')
+
+describe('Testing isOms', function () {
+  it('should return true passing oms', function () {
+    expect(isOms('oms')).toBeTruthy()
+  })
+
+  it('should return true passing smo', function () {
+    expect(isOms('smo')).toBeFalsy()
+  })
+})
